ec6979bc8eceb8355ff7068e24fa2c76c301176a
EinarHelgi <einhelgi@gmail.com>
2015-03-26 16:04:28 +0000

    snuningur kominn með staðsetningar

--- trisis.js
+++ trisis.js
@@ -20,8 +20,11 @@ var floor = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 
 
 var isIbeam = false;
 var isBox = true;
-var count = 200.0;
-var pos = [3, 3, 3];
+var count = 0.0;
+var posL = [0, 0, 0];
+var posM = [2, 2, 2];
+var posR = [0, 0, 0];
+
 var rotX = 0;
 var rotY = 0;
 var rotZ = 0;
@@ -49,7 +52,7 @@ window.onload = function init()
     colorCube();
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.9, 1.0, 1.0, 1.0 );
+    gl.clearColor( 0.5, 0.0, 1.0, 1.0 );
     
     gl.enable(gl.DEPTH_TEST);
 
@@ -59,8 +62,6 @@ window.onload = function init()
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
-    //bufferLine();
-    bufferAxis();
     bufferI();
     bufferL();
     bufferBox();
@@ -104,40 +105,61 @@ window.onload = function init()
     
     // Event listener for keyboard
      window.addEventListener("keydown", function(e){
-        console.log(e.keyCode );
-        console.log(Math.floor(1.3));
+        //console.log(e.keyCode );
         switch( e.keyCode ) {
-            case 65:    // 
+            case 65:    // "A"
                 rotZ += 90;
+                if(rotZ === 360) rotZ = 0;
                 break;
-            case 90:    // 
+            case 90:    // "Z"
                 rotZ -= 90;
+                if(rotZ === -90 ) rotZ = 270
                 break;
-            case 83:    // 
+            case 83:    // "S"
                 rotX += 90;
+                if(rotX === 360) rotX = 0;
                 break;
-            case 88:    // 
+            case 88:    // "X"
                 rotX -= 90;
+                if(rotX === -90 ) rotX = 270
                 break;
-            case 68:    // 
+            case 68:    // "D"
                 rotY += 90;
+                if(rotY === 360) rotY = 0;
                 break;
-            case 67:    // 
+            case 67:    // "C"
                 rotY -= 90;
+                if(rotY === -90 ) rotY = 270
                 break;
             case 37:    // left arrow
-                pos[0]--;
+                posM[0]--;
+                if(posM[0]<0) posM[0] = 0;
                 break;
             case 38:    // up arrow
-                pos[2]--;
+                posM[2]--;
+                if(posM[2]<0) posM[2] = 0;
                 break;
             case 39:    // right arrow
-                pos[0]++;
+                posM[0]++;
+                if(posM[0]>5) posM[0] = 5;
                 break;
             case 40:    // down arrow
-                pos[2]++;
+                posM[2]++;
+                if(posM[2]>5) posM[2] = 5;
                 break;
-         }
+
+            case 66:    // "B" debug grid
+                isBox = !isBox;
+                break;
+            case 84:    // "T" testing
+                //getTiles();
+                newBeam();
+                break;
+        }
+        console.log("x: " + rotX);
+        console.log("y: " + rotY);
+        console.log("z: " + rotZ);
+        getTiles();
      }  );  
 
      // Event listener for keyboard
@@ -187,16 +209,69 @@ function gotToTile(x, y, z, ctm) {
     return ctm;
 }
 
+function getTiles() {
+    if(isIbeam) {
+        posL = [-1,  0,  0];
+        posR = [ 1,  0,  0];
+    }
+    else {
+        posL = [ 0, -1,  0];
+        posR = [ 1,  0,  0];
+
+    }
+
+    posL = calcPos(posL);
+    posR = calcPos(posR);
+    console.log(posL);
+    console.log(posM);
+    console.log(posR);
+    return [posL, posM, posR];
+}
+
+function toRadians (angle) {
+  return angle * (Math.PI / 180);
+}
+
+function calcPos(pos) {
+    var x = toRadians(rotX);
+    var y = toRadians(rotY);
+    var z = toRadians(rotZ);
+    var postemp = [Math.round(pos[0]*Math.cos(z)-pos[1]*Math.sin(z)), 
+                Math.round(pos[0]*Math.sin(z)+pos[1]*Math.cos(z)), 
+                pos[2]];
+    postemp = [Math.round(postemp[2]*Math.sin(y)+postemp[0]*Math.cos(y)), 
+                postemp[1],
+                Math.round(postemp[2]*Math.cos(y)-postemp[0]*Math.sin(y))];
+    postemp = [postemp[0],
+                Math.round(postemp[1]*Math.cos(x)-postemp[2]*Math.sin(x)),
+                Math.round(postemp[1]*Math.sin(x)+postemp[2]*Math.cos(x))];
+    return [posM[0]+postemp[0], posM[1]+postemp[1], posM[2]+postemp[2]];
+}
+
+function newBeam() {
+    var rot = [0, 90, 180, 270];
+    rotX = rot[Math.floor(4*Math.random(4))];
+    rotY = rot[Math.floor(4*Math.random(4))];
+    rotZ = rot[Math.floor(4*Math.random(4))];
+    posM = [3, 1, 3];
+    if(Math.random()<=0.5) isIbeam = !isIbeam;
+    count = 100;
+}
 
 function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     var ctmstack = [];
-    //count++;
-    pos[1] = Math.floor(count/100);
-    if(pos[1] === 19) {
-        console.log("please stop!!");
+    //count+=2;
+    if(Math.floor(count/100) == 1) {
+        posM[1]++;
+        count = 0;
     }
+    /*posM[1] = Math.floor(count/100);
+    if(count > 1950) {
+        newBeam();
+    }*/
+
 
     var ctm = lookAt( vec3(0.0, 0.0, zDist), vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0) );
     ctm = mult( ctm, rotate( parseFloat(spinX), [1, 0, 0] ) );
@@ -206,14 +281,14 @@ function render()
     if(isIbeam) {
         ctm = ctmstack.pop();
         ctmstack.push(ctm);
-        ctm = gotToTile(pos[0], pos[1], pos[2], ctm);
+        ctm = gotToTile(posM[0], posM[1], posM[2], ctm);
         ctm = rotateStuff(rotX, rotY, rotZ, ctm);
         renderI(ctm)
     }
     else {
         ctm = ctmstack.pop();
         ctmstack.push(ctm);
-        ctm = gotToTile(pos[0], pos[1], pos[2], ctm);
+        ctm = gotToTile(posM[0], posM[1], posM[2], ctm);
         ctm = rotateStuff(rotX, rotY, rotZ, ctm);
         renderL(ctm);
     }
